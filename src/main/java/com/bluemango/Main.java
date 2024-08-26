be013fd55417bf7a5d24ab624e16d94f27f4ae52
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-26 16:53:41 -0300

    fourth commit

--- src/main/java/com/bluemango/Main.java
+++ src/main/java/com/bluemango/Main.java
@@ -12,7 +12,19 @@ public class Main {
        String firstName = sc.nextLine(); 
        String secondName = sc.nextLine(); 
        String thirdName = sc.nextLine();
+       char firstChar = firstName.charAt(0);
        System.out.println("Nome completo: " + firstName + " " + secondName + " " + thirdName);
+       System.out.println("Primeira letra do nome: " + firstChar);
+       System.out.print("Digite o primeiro número: ");
+       int num1 = sc.nextInt(); 
+       System.out.print("Digite o segundo número: ");
+       double num2 = sc.nextDouble();
+
+       int sum = (int) ( num1 + num2);
+       System.out.println("Soma dos dois números: " + sum);
+       
+
+
 
 
        sc.close();
