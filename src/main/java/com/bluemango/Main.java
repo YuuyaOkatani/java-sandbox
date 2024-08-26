bdd520dbc3661a72a64052ca44c3a327248bbd4a
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-26 17:16:38 -0300

    exercício corrigido

--- src/main/java/com/bluemango/Main.java
+++ src/main/java/com/bluemango/Main.java
@@ -1,28 +1,23 @@
 package com.bluemango;
 
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        
-       Scanner sc = new Scanner(System.in); 
 
-       System.out.print("Entre com um número inteiro: ");  // solicitando um número inteiro ao usuário
-       int num = sc.nextInt(); 
+        Scanner sc = new Scanner(System.in);
 
-       if(num % 2 ==0){
-        System.out.println("PAR");
-       }
-       else{
-        System.out.println("IMPAR");
-       }
+        System.out.print("Primeiro horário: "); // solicitando um número inteiro ao usuário
+        int a = sc.nextInt();
+        System.out.print("Segundo horário: "); // solicitando um número inteiro ao usuário
+        int b = sc.nextInt();
 
+        if (a % b == 0 || b % a == 0) {
+            System.out.println("São multiplos");
+        } else {
+            System.out.println("Não são multiplos");
+        }
 
-
-
-
-
-       sc.close();
+        sc.close();
     }
 }
