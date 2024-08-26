f8e8ce4109b0494a3f6d44b6a9d3ce0c1838710f
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-26 16:43:48 -0300

    second commit

--- src/main/java/com/bluemango/Main.java
+++ src/main/java/com/bluemango/Main.java
@@ -6,29 +6,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         
-        String product1 = "Computer";
-        String product2 = "Office desk"; 
+       Scanner sc = new Scanner(System.in); 
 
-        int age = 30; 
-        int code = 5290;
+       System.out.print("Digite o primeiro número: ");
+       int a = sc.nextInt(); 
+       System.out.print("Digite o segundo número: ");
+       int b = sc.nextInt();
 
-        char gender = 'F';
+       int sum = a + b;
+       System.out.println("The sum of " + a + " and " + b + " is: " + sum);
 
-        double price1 = 2100.0;
-        double price2 = 650.50; 
-        double measure = 53.234567;
-
-
-        System.out.println("Products:");
-        System.out.println(product1 + " , which price is $" + price1 );
-        System.out.println(product2 + " , which price is $" + price2 );
-        System.out.println("Record: " + age + "years old, code " + code + "and gender: " + gender);
-        System.out.println("Measure with eight decimal places: " + measure);
-        System.out.printf("Rouded (three decimal places): %.3f%n", measure );
-        Locale.setDefault(Locale.US);
-        System.out.printf("US decimal point: %.3f%n", measure);
-
-        
-
+       sc.close();
     }
 }
