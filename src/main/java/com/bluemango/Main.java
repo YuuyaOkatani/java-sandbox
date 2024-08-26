8a3baa6bf530cecddaf0adb31d3e715a219b0e27
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-26 17:08:41 -0300

    exercicio corrigido

--- src/main/java/com/bluemango/Main.java
+++ src/main/java/com/bluemango/Main.java
@@ -8,21 +8,17 @@ public class Main {
         
        Scanner sc = new Scanner(System.in); 
 
-       System.out.println("Digite o primeiro, segundo e terceiro nome: ");
-       String firstName = sc.nextLine(); 
-       String secondName = sc.nextLine(); 
-       String thirdName = sc.nextLine();
-       char firstChar = firstName.charAt(0);
-       System.out.println("Nome completo: " + firstName + " " + secondName + " " + thirdName);
-       System.out.println("Primeira letra do nome: " + firstChar);
-       System.out.print("Digite o primeiro número: ");
-       int num1 = sc.nextInt(); 
-       System.out.print("Digite o segundo número: ");
-       double num2 = sc.nextDouble();
+       System.out.print("Entre com um número inteiro: ");  // solicitando um número inteiro ao usuário
+       int num = sc.nextInt(); 
 
-       int sum = (int) ( num1 + num2);
-       System.out.println("Soma dos dois números: " + sum);
-       
+       if(num < 0){
+        System.out.println("NEGATIVO");
+       }
+       else{
+        System.out.println("NÃO NEGATIVO");
+       }
+
+
 
 
 
