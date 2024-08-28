6bcb0c8e4bc824151b7d4e4119b3b78d1152d896
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-28 20:32:40 -0300

    exercício resolvido

--- src/main/java/com/bluemango/Main.java
+++ src/main/java/com/bluemango/Main.java
@@ -7,38 +7,19 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt(); 
-        int num2 = sc.nextInt();
-        double result = 0;
+        int senha = sc.nextInt(); 
 
-        switch (num) {
-            case 1:
-            result = 4 * num2; 
+        while (senha != 2002) {
+            System.out.println("Senha invalida!");
+            System.out.print("Tente novamente: ");
+            senha = sc.nextInt();
             
-            break;
-            case 2:
-            result = 4.50 * num2; 
-            
-
-            break;
-            case 3:
-            result = 5 * num2;
-
-            break;
-            case 4:
-            result = 2 * num2;
-
-            break;
-            case 5:
-            result = 1.50 * num2;
-
-            break;
-        
         }
 
-        System.out.printf("Total: R$ %.2f%n" , result); // nunca se esqueça disso 
+        System.out.println("Acesso permitido! ");
 
 
+        
      
 
         sc.close();
